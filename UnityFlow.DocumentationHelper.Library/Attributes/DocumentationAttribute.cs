6e0782df3b3200baa76f2973c851ad047e409654
Nuno "Omega Leo" Diogo <nunodiogo@omegaleo.pt>
2024-05-12 21:54:42 +0100

    Migrated project to separate repository

---
+++ UnityFlow.DocumentationHelper.Library/Attributes/DocumentationAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace UnityFlow.DocumentationHelper.Library.Attributes
+{
+    [System.AttributeUsage(AttributeTargets.All, AllowMultiple = false)]
+    public class DocumentationAttribute : Attribute
+    {
+        public string Title;
+        public string Description;
+        public string[]? Args;
+
+        public DocumentationAttribute(string title, string description, string[]? args = null)
+        {
+            Title = title;
+            Description = description;
+            Args = args;
+        }
+    }
+}
